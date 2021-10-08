879d296078151b9b9bf58532f6fd9e08517fe8c0
SMCampos <sandrocampos72@gmail.com>
2021-10-08 19:29:15 -0300

    Criado as funções de manipulação de tabela e atualizado as rotas

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'postgres://qpqojngw:8En06jJZrKxB1a4gcjfLWrqRbKClg_nC@kesavan.db.elephantsql.com/qpqojngw';
 const db = new Pool({ connectionString });
 
 export default db;
