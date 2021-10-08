e4c6b440612cb70c18dd551a7ab1846df4239e0c
SMCampos <sandrocampos72@gmail.com>
2021-10-08 19:30:57 -0300

    Atualizando connectionString

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = 'postgres://qpqojngw:8En06jJZrKxB1a4gcjfLWrqRbKClg_nC@kesavan.db.elephantsql.com/qpqojngw';
+const connectionString = '';
 const db = new Pool({ connectionString });
 
 export default db;
