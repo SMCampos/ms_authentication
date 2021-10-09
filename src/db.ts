2b63dbbde19472fa33371bc6f4d8405e033c2539
SMCampos <sandrocampos72@gmail.com>
2021-10-09 10:43:48 -0300

    Adição de basic authentication e token jwt

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'insira sua url do DB no Elephant';
 const db = new Pool({ connectionString });
 
 export default db;
