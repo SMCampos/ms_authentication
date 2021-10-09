fbc21e11c94f968ecf79105d90bc42dd38058ace
SMCampos <sandrocampos72@gmail.com>
2021-10-09 13:21:09 -0300

    Adicionado tempo de expiração do token

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'insira a url do DB no site Elephant';
 const db = new Pool({ connectionString });
 
 export default db;
